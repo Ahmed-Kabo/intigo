4157c339659ba984105649092f6e476e2cb23630
Ahmed-Kabo <ahmedkabo13@hotmail.com>
2021-08-02 13:18:26 +0200

    add thunk

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createStore } from "redux";
+import { createStore ,applyMiddleware ,compose } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./reducers";
+import thunk from "redux-thunk";
 
+const createComposer =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose ;
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  createComposer(applyMiddleware(thunk))
 );
 ReactDOM.render(
   <React.StrictMode>
